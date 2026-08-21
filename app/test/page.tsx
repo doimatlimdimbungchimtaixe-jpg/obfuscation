76dfbdea785ec51f43f350fc48f72f95f5cbcfb9
Lâm Mod <lammod@obfuscator.dev>
2026-08-20 23:22:03 -0700

    Fix: Remove old test directory

--- app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div className="min-h-screen bg-blue-500 text-white p-8">
-      <h1>Test Page</h1>
-      <p>If you can see this, the build works!</p>
-    </div>
-  );
-}
